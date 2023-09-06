d81029f96791d4119375f27e1c875505ae996864
Advaith Reddy <83079702+advaithreddy@users.noreply.github.com>
2023-09-06 22:15:20 +0530

    Push: Added Cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const express = require('express')
+const cors = require('cors')
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
